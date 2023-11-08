a5a57a9f02ab131515ff0c572197f90b34328dec
iceland017 <128897003+iceland017@users.noreply.github.com>
2023-11-08 22:34:15 +0800

    Add files via upload

---
+++ Extends/src/com/Exercise/Exercise01.java
@@ -0,0 +1,30 @@
+package com.Exercise;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        B b = new B();
+    }
+}
+
+class A {
+    A() {
+        System.out.println("a");
+    }
+
+    A(String name) {
+        System.out.println("a name");
+    }
+}
+
+class B extends A {
+    B() {
+        this("abc");
+        System.out.println("b");
+    }
+
+    B(String name) {
+        super();//默认有super
+        System.out.println("b name");
+    }
+
+}
